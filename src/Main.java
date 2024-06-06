766169c93d994043ec1b8a0c96aa7f995a966ff2
Alice (KhrapinaYA) <khrapinaiuliy188@gmail.com>
2024-06-06 09:59:21 +0300

    Подготовка графической части

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
-        myPanel.setSymbols("1, 2, 3");
+        //myPanel.setSymbols("1, 2, 3");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);

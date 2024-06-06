b6bcb73e50eee16b03f381214e96ac0f64ed4709
Alice (KhrapinaYA) <khrapinaiuliy188@gmail.com>
2024-06-06 09:52:28 +0300

    Подготовка графической части

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        JFrame f = new JFrame();
+        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
+        //myPanel.setSymbols("1, 2, 3");
         f.add(myPanel);
+        f.pack();
         f.setVisible(true);
     }
 }

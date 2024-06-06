3edcbf01177bdc17bdb006c308b3d2fa95763678
Alice (KhrapinaYA) <khrapinaiuliy188@gmail.com>
2024-06-06 09:09:50 +0300

    Подготовка графической части

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyPanel myPanel = new MyPanel();
+        myPanel.repaint();
+        f.add(myPanel);
+        f.setVisible(true);
     }
 }
